a686677d8a27208cf0048e70eb7b0a876f9a8957
kousigan <kousiunleashed@gmail.com>
2020-08-28 23:24:32 +0530

    1 file modified

--- components/galleryItem.jsx
+++ components/galleryItem.jsx
@@ -4,11 +4,12 @@ import Img from 'react-cool-img';
 const GalleryItem =(e)=>{
   const [details,setDetails] = useState({title:e.title,image:e.img});
    
+   const loaderImage = 'https://kousigan.github.io/inkjs/preloader.svg';
    
   return(
     <div className="gItem">
     <span>{details.title}</span> 
-    <Img src={details.image} debounce={1000} placeholder={details.image} cache />
+    <Img src={details.image} debounce={1000} placeholder={loaderImage}  cache />
     <div className="imageDetails"><button className="pure-button" >Like</button></div>
     </div>
   )
