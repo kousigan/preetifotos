4d58e51256afd43e660c0fba3905bde338576d55
kousigan <kousiunleashed@gmail.com>
2020-08-29 22:44:49 +0530

    3 files modified

--- components/logo.jsx
+++ components/logo.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 const Logo = () =>{
   return(
-    <Link to='/' exact >
+    <Link to='/'  >
     <div className="logo">
       <img src="https://firebasestorage.googleapis.com/v0/b/preetifotos-e4812.appspot.com/o/images%2FPreetiPF_logo.svg?alt=media&token=18913d76-d81d-4f4f-98c8-0665bb862b24" alt="Logo" />
       <h1>Preeti<br/>
--- components/post.jsx
+++ components/post.jsx
@@ -1,30 +1,48 @@
-import React, {useState} from 'react';
-import { Link, useParams } from 'react-router-dom';
+import React, { useState } from "react";
+import { Link, useParams } from "react-router-dom";
 import { db } from "../db/config";
+import Img from "react-cool-img";
 
-const Post = () =>{
-  const [post,setPost]=useState();
-    let { id } = useParams();
+const Post = () => {
+  const [check, setLike] = useState(false);
+  const [post, setPost] = useState({});
+  let { id } = useParams();
+  const loaderImage = "https://kousigan.github.io/inkjs/preloader.svg";
 
-    db.collection("photos").doc(id)
-  .get()
-  .then(function(doc) {
-    if (doc.exists) {
-      // setPost(doc.data());
-      console.log("Document data:", doc.data());
-      return;
-     } else {
-      // doc.data() will be undefined in this case
-      console.log("No such document!");
-    }
-  }).catch(function(error) {
-    console.log("Error getting document:", error);
-  });
+  db.collection("photos")
+    .doc(id)
+    .get()
+    .then(function(doc) {
+      if (doc.exists) {
+        setPost(doc.data());
+        // console.log("Document data:", post.title);
+        return;
+      } else {
+        // doc.data() will be undefined in this case
+        console.log("No such document!");
+      }
+    })
+    .catch(function(error) {
+      console.log("Error getting document:", error);
+    });
 
-  return(
-    <div>
-      Post {id}
+
+  return (
+    <div class="pure-g">
+      <div class="pure-u-2-3">
+        <Img src={post.image} className="postImage" placeholder={loaderImage} />
+      </div>
+      <div class="pure-u-1-3">
+        <div class="pure-g">
+          <div class="pure-u-2-3">
+            <h2>{post.title}</h2>
+          </div>
+          <div class="pure-u-1-3 showLikes">
+            <p>{post.like} Likes</p>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 export default Post;
