fe280e106228213c6311d65a08baae585a514958
kousigan <kousiunleashed@gmail.com>
2020-08-29 23:34:13 +0530

    2 files modified

--- components/post.jsx
+++ components/post.jsx
@@ -2,10 +2,14 @@ import React, { useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import { db } from "../db/config";
 import Img from "react-cool-img";
+import { Heart } from 'react-feather';
 
 const Post = () => {
   const [check, setLike] = useState(false);
   const [post, setPost] = useState({});
+  const [comment,setComment]=useState({
+      content: ''
+  });
   let { id } = useParams();
   const loaderImage = "https://kousigan.github.io/inkjs/preloader.svg";
 
@@ -27,20 +31,29 @@ const Post = () => {
     });
 
 
+  const handleDetails = e => {
+    setBook({ ...book, [e.target.name]: e.target.value });
+  };
   return (
     <div class="pure-g">
-      <div class="pure-u-2-3">
-        <Img src={post.image} className="postImage" placeholder={loaderImage} />
-      </div>
-      <div class="pure-u-1-3">
-        <div class="pure-g">
+      <div class="pure-u-2-3 postImageContainer">
+      <div class="pure-g">
           <div class="pure-u-2-3">
             <h2>{post.title}</h2>
           </div>
           <div class="pure-u-1-3 showLikes">
-            <p>{post.like} Likes</p>
+            <p>{post.like} Likes</p> <button className={check ? 'liked like pure-button' : 'like pure-button'} ><Heart/></button>
           </div>
         </div>
+        <Img src={post.image} className="postImage" placeholder={loaderImage} />
+      </div>
+      <div class="pure-u-1-3">
+        <h2>Comments</h2>
+        <form class="pure-form comments">
+           <fieldset>
+                     <textarea type="text" placeholder="Enter comments" className="enterComments"></textarea>
+          </fieldset>
+        </form>
       </div>
     </div>
   );
